2173ca5bfad2b4a34561ca3ae1d6167e70fd2d14
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-06-21 21:51:48 +0200

    all up and runnug

--- sky_scrapers/views.py
+++ sky_scrapers/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Skyscraper
+from .serializers import SkyscraperSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
+
+class SkyscraperList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Skyscraper.objects.all()
+    serializer_class = SkyscraperSerializer
+
+class SkyscraperDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Skyscraper.objects.all()
+    serializer_class = SkyscraperSerializer
+
